f0965b5af293ebf429cd18abcbc842850db6f87b
Marlaina <mlwunder@mtu.edu>
2020-08-19 11:42:45 -0500

    did area of a circle studio

---
+++ src/org/launchcode/java/studios/areaofacircle/Circle.java
@@ -0,0 +1,13 @@
+package org.launchcode.java.studios.areaofacircle;
+
+public class Circle {
+
+    public static void main(String[] args){
+
+    }
+
+    public static double getArea(Double radius){
+        return Math.PI * radius * radius;
+    }
+
+}
